7c30a31b7ca67159c14d0b1bda06ec1966b80aae
suneettipirneni <suneettipirneni@icloud.com>
2023-11-18 15:40:08 -0500

    use optimizer

--- detection/general_detection/train.py
+++ detection/general_detection/train.py
@@ -6,10 +6,10 @@ from torchvision.models.resnet import ResNet, Bottleneck, resnet50
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from tqdm import tqdm
-from torch import nn
+from torch import nn, optim
 import numpy as np
 
-def train_one_epoch(model: ResNet, device: torch.device, criterion, data_loader: DataLoader):
+def train_one_epoch(model: ResNet, device: torch.device, criterion, data_loader: DataLoader, optimizer):
   if not model.training:
     raise Exception("Model must be in training mode in order to train.")
   
@@ -20,6 +20,8 @@ def train_one_epoch(model: ResNet, device: torch.device, criterion, data_loader:
     input, target = batch
 
     input, target = input.to(device), target.to(device)
+
+    optimizer.zero_grad()
 
     # Forward pass of input
     output = model(input)
@@ -32,7 +34,7 @@ def train_one_epoch(model: ResNet, device: torch.device, criterion, data_loader:
 
     losses.append(loss.item())
 
-    print(target)
+    optimizer.step()
 
     pred = output.argmax(dim=1, keepdim=True)
     total += len(target)
@@ -62,10 +64,12 @@ if __name__ == '__main__':
   train_dataset = V2Dataset(cam1_path="./data/v2_cam1_cam2_split_by_driver/Camera 1/train", cam2_path="./data/v2_cam1_cam2_split_by_driver/Camera 2/train", transform=transform)
   train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
+  optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
   epochs = 100
 
   for epoch in tqdm(range(epochs)):
-    loss, train_acc = train_one_epoch(model=model, device=device, data_loader=train_dataloader, criterion=criterion)  
+    loss, train_acc = train_one_epoch(model=model, device=device, data_loader=train_dataloader, criterion=criterion, optimizer=optimizer)  
 
     # Log statistics
     print(f"Epoch {epoch} loss = {float(np.mean(loss))}, train accuracy = {train_acc}")
