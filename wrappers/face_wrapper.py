acddc34ae1c7ad306249ece00dc13583d9406814
Perl-R <robinaaperl@gmail.com>
2023-11-18 13:06:16 -0500

    Add Face Inference Wrapper (minor adjustments to face cnn)

---
+++ wrappers/face_wrapper.py
@@ -0,0 +1,25 @@
+import cv2
+import torch
+from torch import nn 
+from torchvision.transforms import v2
+from ultralytics import YOLO
+
+from cnn.face_cnn import extract_face_detections
+
+class Face_Inference_Wrapper(nn.Module):
+    def __init__(self, model):
+        super(Face_Inference_Wrapper, self).__init__()
+        self.detector = YOLO('../detection/face_detection/pretrained_models/yolov8n-face.pt')
+        self.model = model
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+    def forward(self, x):
+        detections = self.detector(x)
+        rois = extract_face_detections(detections)
+        rois = self.model.preprocessor(rois)
+        rois.to(self.device)
+        return self.model(rois)
+
+
+
