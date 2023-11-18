d177ccc0203aec32d67c03413124a95939847e95
suneettipirneni <suneettipirneni@icloud.com>
2023-11-18 15:28:58 -0500

    fixes

--- detection/general_detection/train.py
+++ detection/general_detection/train.py
@@ -9,14 +9,14 @@ from tqdm import tqdm
 from torch import nn
 import numpy as np
 
-def train_one_epoch(model: ResNet, device: torch.device, criterion, epoch: int, data_loader: DataLoader):
+def train_one_epoch(model: ResNet, device: torch.device, criterion, data_loader: DataLoader):
   if not model.training:
     raise Exception("Model must be in training mode in order to train.")
   
   losses = []
   correct, total = 0, 0
 
-  for batch in tqdm(train_dataloader):
+  for batch in tqdm(data_loader):
     input, target = batch
 
     input, target = input.to(device), target.to(device)
@@ -32,12 +32,14 @@ def train_one_epoch(model: ResNet, device: torch.device, criterion, epoch: int, 
 
     losses.append(loss.item())
 
+    print(target)
+
     pred = output.argmax(dim=1, keepdim=True)
     total += len(target)
 
     correct += pred.eq(target.view_as(pred)).sum().item()
 
-  train_loss = train_loss = float(np.mean(losses))
+  train_loss = float(np.mean(losses))
   train_acc = (correct / total) * 100
 
   return train_loss, train_acc
@@ -45,10 +47,9 @@ def train_one_epoch(model: ResNet, device: torch.device, criterion, epoch: int, 
 
 if __name__ == '__main__':
   model = resnet50(num_classes=10)
-  model.train()
   device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
   model.to(device)
+  model.train()
 
   transform = transforms.Compose([
         transforms.Resize((224,224)),
@@ -58,13 +59,18 @@ if __name__ == '__main__':
 
   criterion = nn.CrossEntropyLoss()
 
-  train_dataset = V2Dataset(cam1_path="./data/v2_cam1_cam2_split_by_driver/Camera 1", cam2_path="./data/v2_cam1_cam2_split_by_driver/Camera 2", transform=transform)
+  train_dataset = V2Dataset(cam1_path="./data/v2_cam1_cam2_split_by_driver/Camera 1/train", cam2_path="./data/v2_cam1_cam2_split_by_driver/Camera 2/train", transform=transform)
   train_dataloader = DataLoader(train_dataset, batch_size=64)
 
   epochs = 100
 
   for epoch in tqdm(range(epochs)):
-    loss, train_acc = train_one_epoch(model=model, device=device, data_loader=train_dataloader, criterion=criterion, epoch=epoch)
+    # loss, train_acc = train_one_epoch(model=model, device=device, data_loader=train_dataloader, criterion=criterion)
+
+    for batch in tqdm(train_dataloader):
+      input, target = batch
+      
+      print(target)
 
     # Log statistics
     print(f"Epoch {epoch} loss = {float(np.mean(loss))}, train accuracy = {train_acc}")
