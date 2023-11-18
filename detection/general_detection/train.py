c9eeb92813bc1452c432ecd7a12431a0770c8511
suneettipirneni <suneettipirneni@icloud.com>
2023-11-18 16:11:04 -0500

    add validation and saving

--- detection/general_detection/train.py
+++ detection/general_detection/train.py
@@ -46,6 +46,32 @@ def train_one_epoch(model: ResNet, device: torch.device, criterion, data_loader:
 
   return train_loss, train_acc
 
+def val(model: ResNet, device: torch.device, test_loader: DataLoader, criterion, epoch: int):
+  if model.training:
+    raise Exception("Model must be in eval mode in order to validate.")
+  
+  losses = []
+  correct, total = 0, 0
+
+  with torch.no_grad():
+    for sample in tqdm(test_loader):
+      input, target = sample
+      input, target = input.to(device), target.to(device)
+
+      output = model(input)
+      
+      loss = criterion(output, target)
+      losses.append(loss)
+
+      pred = output.argmax(dim=1, keepdim=True)
+      total += len(target)
+      correct += pred.eq(target.view_as(pred)).sum().item()
+
+  test_loss = float(np.mean(losses))
+  accuracy = 100. * (correct / total)
+
+  
+  return test_loss, accuracy
 
 if __name__ == '__main__':
   model = resnet50(num_classes=10)
@@ -64,12 +90,23 @@ if __name__ == '__main__':
   train_dataset = V2Dataset(cam1_path="./data/v2_cam1_cam2_split_by_driver/Camera 1/train", cam2_path="./data/v2_cam1_cam2_split_by_driver/Camera 2/train", transform=transform)
   train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
+  test_dataset = V2Dataset(cam1_path="./data/v2_cam1_cam2_split_by_driver/Camera 1/test", cam2_path="./data/v2_cam1_cam2_split_by_driver/Camera 2/test", transform=transform)
+  test_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+
   optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
   epochs = 100
 
   for epoch in tqdm(range(epochs)):
     loss, train_acc = train_one_epoch(model=model, device=device, data_loader=train_dataloader, criterion=criterion, optimizer=optimizer)  
+
+    if epoch % 5 == 0:
+      model.eval()
+      val(model=model, device=device, test_loader=test_dataloader, criterion=criterion, epoch=epoch)      
+      model.train()
+
+    if epoch % 10 == 0:
+      torch.save(model.state_dict(), f"general-detect-{epoch}.pt")
 
     # Log statistics
     print(f"Epoch {epoch} loss = {float(np.mean(loss))}, train accuracy = {train_acc}")
