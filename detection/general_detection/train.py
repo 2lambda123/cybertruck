0b1b28420aa3578ac6705d2488c35fdb1266104e
suneettipirneni <suneettipirneni@icloud.com>
2023-11-19 15:26:29 -0500

    add augmentations

--- detection/general_detection/train.py
+++ detection/general_detection/train.py
@@ -82,9 +82,14 @@ if __name__ == '__main__':
   model.train()
 
   transform = transforms.Compose([
-        transforms.Resize((224,224)),
+        # transforms.Resize((224,224)),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
         transforms.ToTensor(),
-        transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+        transforms.RandomRotation(degrees=15),
+        transforms.RandomResizedCrop(size=(224, 224)),
+        # mean = tensor([0.3357, 0.3277, 0.3343])
+        # std = tensor([0.3447, 0.3375, 0.3347])
+        transforms.Normalize((0.3357,0.3277,0.3343), (0.3447,0.3375,0.3347))
   ])
 
   criterion = nn.CrossEntropyLoss()
