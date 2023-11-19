1362ad354b17fe6ad6c008226e500ff3125981fc
suneettipirneni <suneettipirneni@icloud.com>
2023-11-19 16:15:52 -0500

    use proper rank

--- detection/general_detection/train.py
+++ detection/general_detection/train.py
@@ -134,12 +134,12 @@ if __name__ == '__main__':
   for epoch in tqdm(range(epochs), unit="epoch"):
     loss, train_acc = train_one_epoch(model=model, device=device, data_loader=train_dataloader, criterion=criterion, optimizer=optimizer)  
 
-    if (epoch + 1) % 5 == 0:
+    if (epoch + 1) % 5 == 0 and rank == 0:
       model.eval()
       val(model=model, device=device, test_loader=test_dataloader, criterion=criterion, epoch=epoch)      
       model.train()
 
-    if (epoch + 1) % 10 == 0:
+    if (epoch + 1) % 10 == 0 and rank == 0:
       torch.save(model.state_dict(), f"general-detect-{epoch}.pt")
 
     # Log statistics
