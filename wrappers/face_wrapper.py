e71e0ce54c855ae26f0551f71beacbd1bc2da535
roncamposj <roncamposj@knights.ucf.edu>
2023-11-30 17:22:25 -0500

    .

--- wrappers/face_wrapper.py
+++ wrappers/face_wrapper.py
@@ -8,9 +8,9 @@ from torchvision.transforms import v2
 from cnn.face_cnn import extract_face_detections
 
 class Face_Inference_Wrapper(nn.Module):
-    def __init__(self, model):
+    def __init__(self, model, detector_path):
         super(Face_Inference_Wrapper, self).__init__()
-        self.detector = YOLO('/home/ron/Classes/CV-Systems/cybertruck/detection/face_detection/pretrained_models/yolov8n-face.pt')
+        self.detector = YOLO(detector_path)
         self.model = model
         self.model.eval()
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
